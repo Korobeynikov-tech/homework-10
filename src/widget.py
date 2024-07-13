b0c9833a4fb4843617ca09b8e5cd50848d884a14
Dimuil <korob-54@mail.ru>
2024-07-13 14:16:10 +0300

    Adding the outputs of the function

--- src/widget.py
+++ src/widget.py
@@ -25,15 +25,19 @@ def mask_account_card(info):
     if 'Счет' in info:
         # Маскировка номера счета
         account_number = info.split()[-1]
-        masked_number = mask_account_number(account_number)
+        masked_number = get_mask_card_number(account_number)
         return info.replace(account_number, masked_number)
     else:
         # Маскировка номера карты
         card_number = info.split()[-1]
-        masked_number = mask_card_number(card_number)
+        masked_number = get_mask_account(card_number)
         return info.replace(card_number, masked_number)
 
 # Примеры использования функции
-print(mask_account_card("Visa Platinum 7000792289606361"))  # Вывод: Visa Platinum 7000********6361
-print(mask_account_card("Maestro 7000792289606361"))        # Вывод: Maestro 7000********6361
-print(mask_account_card("Счет 73654108430135874305"))       # Вывод: Счет ****************4305
+print(mask_account_card("Maestro 1596837868705199")) # Maestro ************5199
+print(mask_account_card("Счет 64686473678894779589")) # Счет ****************9589
+print(mask_account_card("MasterCard 7158300734726758")) # MasterCard ************6758
+print(mask_account_card("Счет 35383033474447895560")) # Счет ****************5560
+print(mask_account_card("Visa Classic 6831982476737658")) # Visa Classic ************7658
+print(mask_account_card("Visa Platinum 8990922113665229")) # Visa Platinum ************5229
+print(mask_account_card("Счет 73654108430135874305")) # Счет ****************4305
