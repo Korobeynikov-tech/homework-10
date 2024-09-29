7f91c1545a05af0423740e639d3efeaf099226bd
Dimuil <korob-54@mail.ru>
2024-09-29 11:55:02 +0300

    new updates 29.09

--- src/generators.py
+++ src/generators.py
@@ -3,7 +3,7 @@ def filter_by_currency(transactions, currency):
     Функция фильтрует список транзакций по валюте и возвращает только те транзакции, у которых валюта совпадает с заданной.
     """
     for transaction in transactions:
-        if transaction.get('currency') == currency:
+        if transaction['operationAmount']['currency']['name'] == currency:
             yield transaction
 
 
@@ -15,9 +15,10 @@ def transaction_descriptions(transactions):
         yield transaction.get('description')
 
 
-def card_number_generator(start, stop):
+def card_number_generator(start: int, stop: int):
     """
     Функция-генератор генерирует последовательность номеров карт от start до stop.
     """
-    for card_number in range(start, stop):
-        yield card_number
+    for i in range(start, stop):
+        result = str(i).zfill(16)
+        yield ' '.join([result[0:4], result[4:8], result[8:12], result[12:16]])
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -4,14 +4,57 @@ from src.generators import card_number_generator
 
 
 def test_filter_by_currency():
-    transactions = [{'currency': 'USD', 'amount': 100}, {'currency': 'EUR', 'amount': 50},
-                    {'currency': 'USD', 'amount': 75}]
+    transactions = [{
+          "id": 939719570,
+          "state": "EXECUTED",
+          "date": "2018-06-30T02:08:58.425572",
+          "operationAmount": {
+              "amount": "9824.07",
+              "currency": {
+                  "name": "USD",
+                  "code": "USD"
+              }
+          },
+          "description": "Перевод организации",
+          "from": "Счет 75106830613657916952",
+          "to": "Счет 11776614605963066702"
+      },
+      {
+              "id": 142264268,
+              "state": "EXECUTED",
+              "date": "2019-04-04T23:20:05.206878",
+              "operationAmount": {
+                  "amount": "79114.93",
+                  "currency": {
+                      "name": "USD",
+                      "code": "USD"
+                  }
+              },
+              "description": "Перевод со счета на счет",
+              "from": "Счет 19708645243227258542",
+              "to": "Счет 75651667383060284188"
+       },{
+              "id": 143264268,
+              "state": "EXECUTED",
+              "date": "2019-04-04T23:20:05.206878",
+              "operationAmount": {
+                  "amount": "79114.93",
+                  "currency": {
+                      "name": "RUB",
+                      "code": "RUB"
+                  }
+              },
+              "description": "Перевод со счета на счет",
+              "from": "Счет 19708645243227258542",
+              "to": "Счет 75651667383060284188"
+       }]
+
     currency = 'USD'
 
     # filtered_transactions = list(filter_by_currency(transactions, currency))
     generator = filter_by_currency(transactions, currency)
     assert next(generator) == transactions[0]
-    assert next(generator) == transactions[2]
+    assert next(generator) == transactions[1]
 
 
 def test_transaction_descriptions():
@@ -30,6 +73,6 @@ def test_card_number_generator():
 
     # card_numbers = list(card_number_generator(start, stop))
     generator = card_number_generator(start, stop)
-    assert next(generator) == 1000
-    assert next(generator) == 1001
-    assert next(generator) == 1002
+    assert next(generator) == "0000 0000 0000 1000"
+    assert next(generator) == "0000 0000 0000 1001"
+    assert next(generator) == "0000 0000 0000 1002"
